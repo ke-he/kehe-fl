9f96bf117da347ff450262858c3a7f5d9ffd5254
Kevin Hetzenauer <kevin@hetzenauer.me>
2025-06-16 22:12:20 +0200

    fix: readme

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ def get_version():
     with open("kehe_fl/__init__.py", "r") as f:
         return re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="kehe-fl",
     version=get_version(),
     description="A federated learning package for IoT devices and aggregation server communication.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
         "aiomqtt"
